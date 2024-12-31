7751d50978ea1a6dd3e62cb3cc4dfb080f11486f
Rahul <rahulrashmi.porwal@gmail.com>
2024-12-31 14:52:39 +0530

    Back Button Added in Xo

--- src/app/games/xo/xo-home.tsx
+++ src/app/games/xo/xo-home.tsx
@@ -1,11 +1,16 @@
 import Header from "@/app/shared/header";
 import Board from "./board";
+import Button from "@/app/shared/button";
+import Link from "next/link";
 
 const XoHome = () => {
   return (
     <div>
       <Header heading={"Welcome to RP's TicTac"} />
       <Board size={5} />
+      <Link href={"/"}>
+        <Button text={"Back to Home"} />
+      </Link>
     </div>
   );
 };
--- src/app/shared/button.tsx
+++ src/app/shared/button.tsx
@@ -9,15 +9,15 @@ const Button = ({
   onClick,
 }: {
   text?: string;
-  actionId: string;
+  actionId?: string;
   icon?: string;
   children?: Object;
-  onClick?: (actionId: string) => void;
+  onClick?: (actionId?: string) => void;
 }) => {
   return (
     <>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-gradient-to-r from-amber-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => onClick && onClick(actionId)}
       >
         {text}
